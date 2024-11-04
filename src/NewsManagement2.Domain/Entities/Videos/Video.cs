872b1e17f08858dbeac3f9e80b7a677ce72ceacd
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-11-04 21:36:15 +0300

    video Entity

---
+++ src/NewsManagement2.Domain/Entities/Videos/Video.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsManagement2.Entities.Videos
+{
+    public class Video //: ListableContent
+    {
+       // public VideoType VideoType { get; set; }
+        public string? Url { get; set; }
+        public Guid? VideoId { get; set; }
+
+
+        public Video() { }
+
+    }
+}
