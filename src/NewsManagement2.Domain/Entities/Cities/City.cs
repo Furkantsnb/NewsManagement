2590ad3ce1f76c91ee175a2d60fff100845ca196
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-11-04 21:27:50 +0300

    City Entity

---
+++ src/NewsManagement2.Domain/Entities/Cities/City.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.MultiTenancy;
+
+namespace NewsManagement2.Entities.Cities
+{
+    public class City : FullAuditedAggregateRoot<int>, IMultiTenant
+    {
+
+        public Guid? TenantId { get; set; }
+        public string CityName { get; set; }
+        public int CityCode { get; set; }
+        // public List<ListableContentCity> ListableContentCities { get; set; }
+
+        internal City()
+        {
+
+        }
+
+    }
+}
