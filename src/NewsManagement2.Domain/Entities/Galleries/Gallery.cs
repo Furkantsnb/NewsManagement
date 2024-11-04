10512fd3bdd7c59abfb56657aa7219a8d92faefe
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-11-04 21:42:27 +0300

    Gallery Entity - GalleryImages Entity

---
+++ src/NewsManagement2.Domain/Entities/Galleries/Gallery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsManagement2.Entities.Galleries
+{
+    public class Gallery //: ListableContent
+    {
+       public List<GalleryImage> GalleryImages { get; set; }
+
+        public Gallery() { }
+    }
+}
---
+++ src/NewsManagement2.Domain/Entities/Galleries/GalleryImages.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.MultiTenancy;
+
+namespace NewsManagement2.Entities.Galleries
+{
+    public class GalleryImage : Entity, IMultiTenant
+    {
+        public Guid? TenantId { get; set; }
+        public int Order { get; set; }
+        public string NewsContent { get; set; }
+        public Guid ImageId { get; set; }
+        public int GalleryId { get; set; }
+        public Gallery Gallery { get; set; }
+
+
+        public GalleryImage() { }
+
+        public override object[] GetKeys()
+        {
+            return new object[] { GalleryId, ImageId };
+        }
+    }
+}
