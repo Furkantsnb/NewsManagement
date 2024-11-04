0d9a87fcfa982b114c43c5031ef342ee02206ea9
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-11-04 21:30:11 +0300

    Tag Entity

---
+++ src/NewsManagement2.Domain/Entities/Tags/Tag.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.MultiTenancy;
+
+namespace NewsManagement2.Entities.Tags
+{
+    public class Tag : FullAuditedAggregateRoot<int>, IMultiTenant
+    {
+        public Guid? TenantId { get; set; }
+        public string TagName { get; set; }
+
+      //  public ICollection<ListableContentTag> ListableContentTags { get; set; }
+
+
+        internal Tag()
+        {
+        }
+
+
+    }
+}
