787ef51e852ff426b12bee85a100485264a841fb
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-11-05 17:49:04 +0300

    StatusType Enum - ListableContentType Enum

---
+++ src/NewsManagement2.Domain.Shared/EntityConsts/ListableContentConsts/StatusType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsManagement2.EntityConsts.ListableContentConsts
+{
+    public enum StatusType
+    {
+        Draft,          //0 Taslak  
+        Archived,       //1 Arşivlenmiş 
+        Published,      //2 Yayınlanmış  
+        Scheduled,      //3 Planlanmış 
+        Deleted,        //4 Silinmiş  
+    }
+}
