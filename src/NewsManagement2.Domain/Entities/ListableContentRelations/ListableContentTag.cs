bf11b92366cc54a211ddd0db36c53f5833d7b527
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-11-05 14:53:34 +0300

    ListableContentTag Entity

---
+++ src/NewsManagement2.Domain/Entities/ListableContentRelations/ListableContentTag.cs
@@ -0,0 +1,29 @@
+﻿using NewsManagement2.Entities.ListableContents;
+using NewsManagement2.Entities.Tags;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.MultiTenancy;
+
+namespace NewsManagement2.Entities.ListableContentRelations
+{
+    public class ListableContentTag : Entity, IMultiTenant
+    {
+        public Guid? TenantId { get; set; }
+        public int TagId { get; set; }
+        public Tag Tag { get; set; }
+        public int ListableContentId { get; set; }
+        public ListableContent ListableContent { get; set; }
+
+
+        internal ListableContentTag() { }
+
+        public override object[] GetKeys()
+        {
+            return new object[] { TagId, ListableContentId };
+        }
+    }
+}
