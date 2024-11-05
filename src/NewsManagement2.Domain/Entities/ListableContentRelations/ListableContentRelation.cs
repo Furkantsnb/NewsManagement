4e404b103aa4b1802d4906ff49d4cc8851111546
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-11-05 14:52:52 +0300

    ListableContentRelation

---
+++ src/NewsManagement2.Domain/Entities/ListableContentRelations/ListableContentRelation.cs
@@ -0,0 +1,29 @@
+﻿using NewsManagement2.Entities.ListableContents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.MultiTenancy;
+
+namespace NewsManagement2.Entities.ListableContentRelations
+{
+    public class ListableContentRelation : Entity, IMultiTenant
+    {
+        public Guid? TenantId { get; set; }
+        public int ListableContentId { get; set; }
+        public ListableContent ListableContent { get; set; }
+
+        public int RelatedListableContentId { get; set; }
+        public ListableContent RelatedListableContent { get; set; }
+
+        internal ListableContentRelation() { }
+
+
+        public override object[] GetKeys()
+        {
+            return new object[] { ListableContentId, RelatedListableContentId };
+        }
+    }
+}
