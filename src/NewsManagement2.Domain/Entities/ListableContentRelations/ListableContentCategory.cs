45b73d2d7d81c1c615326826e1ef23acb3a9fac9
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-11-05 14:50:09 +0300

    ListableContentCategory Entity

---
+++ src/NewsManagement2.Domain/Entities/ListableContentRelations/ListableContentCategory.cs
@@ -0,0 +1,32 @@
+﻿using NewsManagement2.Entities.Categories;
+using NewsManagement2.Entities.ListableContents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.MultiTenancy;
+
+namespace NewsManagement2.Entities.ListableContentRelations
+{
+    public class ListableContentCategory : Entity, IMultiTenant
+    {
+        public Guid? TenantId { get; set; }
+        public bool IsPrimary { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public int ListableContentId { get; set; }
+        public ListableContent ListableContent { get; set; }
+
+
+
+        internal ListableContentCategory() { }
+
+        public override object[] GetKeys()
+        {
+            return new object[] { CategoryId, ListableContentId };
+        }
+
+    }
+}
