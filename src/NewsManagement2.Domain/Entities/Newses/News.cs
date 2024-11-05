7e94bd5f41e12d3aa1b7c905b0d57cd6f86140bc
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-11-05 14:56:35 +0300

    News Entity - NewsDetailManage Entity

---
+++ src/NewsManagement2.Domain/Entities/Newses/News.cs
@@ -0,0 +1,16 @@
+﻿using NewsManagement2.Entities.ListableContents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsManagement2.Entities.Newses
+{
+    public class News : ListableContent
+    {
+        public List<NewsDetailImage> DetailImageIds { get; set; }
+
+        public News() { }
+    }
+}
