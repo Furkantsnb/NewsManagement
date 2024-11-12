ff1c4b8bf7953d51f62820f97870a8fdea1ea381
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-11-12 09:32:10 +0300

    File Management modulu eklendi

--- src/NewsManagement2.EntityFrameworkCore/EntityFrameworkCore/NewsManagement2DbContext.cs
+++ src/NewsManagement2.EntityFrameworkCore/EntityFrameworkCore/NewsManagement2DbContext.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using NewsManagement2.Entities.Categories;
+using NewsManagement2.Entities.Cities;
+using NewsManagement2.Entities.Galleries;
+using NewsManagement2.Entities.ListableContentRelations;
+using NewsManagement2.Entities.ListableContents;
+using NewsManagement2.Entities.Newses;
+using NewsManagement2.Entities.Tags;
+using NewsManagement2.Entities.Videos;
 using Volo.Abp.AuditLogging.EntityFrameworkCore;
 using Volo.Abp.BackgroundJobs.EntityFrameworkCore;
 using Volo.Abp.Data;
 using Volo.Abp.DependencyInjection;
 using Volo.Abp.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore.Modeling;
 using Volo.Abp.FeatureManagement.EntityFrameworkCore;
 using Volo.Abp.Identity;
 using Volo.Abp.Identity.EntityFrameworkCore;
@@ -23,20 +32,22 @@ public class NewsManagement2DbContext :
     IIdentityDbContext,
     ITenantManagementDbContext
 {
-    /* Add DbSet properties for your Aggregate Roots / Entities here. */
+
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<News> Newses { get; set; }
+    public DbSet<City> Cities { get; set; }
+    public DbSet<Video> Videos { get; set; }
+    public DbSet<Gallery> Galleries { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<GalleryImage> GalleryImages { get; set; }
+    public DbSet<NewsDetailImage> NewsDetailImages { get; set; }
+    public DbSet<ListableContent> ListableContents { get; set; }
+    public DbSet<ListableContentTag> ListableContentTags { get; set; }
+    public DbSet<ListableContentCity> ListableContentCities { get; set; }
+    public DbSet<ListableContentRelation> ListableContentRelations { get; set; }
+    public DbSet<ListableContentCategory> ListableContentCategories { get; set; }
 
     #region Entities from the modules
-
-    /* Notice: We only implemented IIdentityDbContext and ITenantManagementDbContext
-     * and replaced them for this DbContext. This allows you to perform JOIN
-     * queries for the entities of these modules over the repositories easily. You
-     * typically don't need that for other modules. But, if you need, you can
-     * implement the DbContext interface of the needed module and use ReplaceDbContext
-     * attribute just like IIdentityDbContext and ITenantManagementDbContext.
-     *
-     * More info: Replacing a DbContext of a module ensures that the related module
-     * uses this DbContext on runtime. Otherwise, it will use its own DbContext class.
-     */
 
     //Identity
     public DbSet<IdentityUser> Users { get; set; }
@@ -73,14 +84,123 @@ public class NewsManagement2DbContext :
         builder.ConfigureOpenIddict();
         builder.ConfigureFeatureManagement();
         builder.ConfigureTenantManagement();
+       // builder.ConfigureFileManagement();
+        #region Gallery, Video, News
+        builder.Entity<Gallery>(b =>
+        {
 
-        /* Configure your own tables/entities inside here */
+            b.ToTable(NewsManagement2Consts.DbTablePrefix + "Galleries", NewsManagement2Consts.DbSchema);
+            b.ConfigureByConvention();
+        });
 
-        //builder.Entity<YourEntity>(b =>
-        //{
-        //    b.ToTable(NewsManagement2Consts.DbTablePrefix + "YourEntities", NewsManagement2Consts.DbSchema);
-        //    b.ConfigureByConvention(); //auto configure for the base class props
-        //    //...
-        //});
+        builder.Entity<GalleryImage>(b =>
+        {
+            b.HasKey(x => new { x.GalleryId, x.ImageId });
+
+            b.HasOne(x => x.Gallery).WithMany(x => x.GalleryImages).HasForeignKey(x => x.GalleryId);
+
+
+            b.ToTable(NewsManagement2Consts.DbTablePrefix + "GalleryImages", NewsManagement2Consts.DbSchema);
+            b.ConfigureByConvention();
+        });
+
+        builder.Entity<NewsDetailImage>(b =>
+        {
+            b.HasKey(x => new { x.NewsId, x.DetailImageId });
+
+            b.HasOne(x => x.News).WithMany(x => x.DetailImageIds).HasForeignKey(x => x.NewsId);
+
+            b.ToTable(NewsManagement2Consts.DbTablePrefix + "NewsDetailImages", NewsManagement2Consts.DbSchema);
+            b.ConfigureByConvention();
+        });
+
+        builder.Entity<News>(b =>
+        {
+
+            b.ToTable(NewsManagement2Consts.DbTablePrefix + "Newses", NewsManagement2Consts.DbSchema);
+            b.ConfigureByConvention();
+        });
+
+        builder.Entity<Video>(b =>
+        {
+
+            b.ToTable(NewsManagement2Consts.DbTablePrefix + "Videos", NewsManagement2Consts.DbSchema);
+            b.ConfigureByConvention();
+        });
+
+        #endregion
+
+        #region ListableContent
+
+        builder.Entity<ListableContentRelation>(b =>
+        {
+            b.HasKey(x => new { x.ListableContentId, x.RelatedListableContentId });
+
+            b.HasOne(x => x.ListableContent).WithMany(x => x.ListableContentRelations).HasForeignKey(x => x.ListableContentId);//.OnDelete(DeleteBehavior.Restrict);
+            b.HasOne(x => x.RelatedListableContent).WithMany().HasForeignKey(x => x.RelatedListableContentId);//.OnDelete(DeleteBehavior.Restrict);
+
+            b.ToTable(NewsManagement2Consts.DbTablePrefix + "ListableContentRelations", NewsManagement2Consts.DbSchema);
+            b.ConfigureByConvention();
+        });
+
+        #endregion
+
+        #region ListableContent(Tag, City, Category)
+        builder.Entity<ListableContentTag>(b =>
+        {
+            b.HasKey(x => new { x.ListableContentId, x.TagId });
+            b.HasOne(x => x.ListableContent).WithMany(x => x.ListableContentTags).HasForeignKey(x => x.ListableContentId);
+            b.HasOne(x => x.Tag).WithMany(x => x.ListableContentTags).HasForeignKey(x => x.TagId);
+
+            b.ToTable(NewsManagement2Consts.DbTablePrefix + "ListableContentTags", NewsManagement2Consts.DbSchema);
+            b.ConfigureByConvention();
+        });
+
+        builder.Entity<ListableContentCity>(b =>
+        {
+            b.HasKey(x => new { x.ListableContentId, x.CityId });
+            b.HasOne(x => x.ListableContent).WithMany(x => x.ListableContentCities).HasForeignKey(x => x.ListableContentId);
+            b.HasOne(x => x.City).WithMany(x => x.ListableContentCities).HasForeignKey(x => x.CityId);
+
+            b.ToTable(NewsManagement2Consts.DbTablePrefix + "ListableContentCities", NewsManagement2Consts.DbSchema);
+            b.ConfigureByConvention();
+        });
+
+        builder.Entity<ListableContentCategory>(b =>
+        {
+            b.HasKey(x => new { x.ListableContentId, x.CategoryId });
+            b.HasOne(x => x.ListableContent).WithMany(x => x.ListableContentCategories).HasForeignKey(x => x.ListableContentId);
+            b.HasOne(x => x.Category).WithMany(x => x.ListableContentCategories).HasForeignKey(x => x.CategoryId);
+
+            b.ToTable(NewsManagement2Consts.DbTablePrefix + "ListableContentCategories", NewsManagement2Consts.DbSchema);
+            b.ConfigureByConvention();
+        });
+        #endregion
+
+        #region Tag, City, Category
+        builder.Entity<Tag>(b =>
+        {
+            b.HasMany(x => x.ListableContentTags).WithOne(x => x.Tag).HasForeignKey(x => x.TagId).IsRequired();
+
+            b.ToTable(NewsManagement2Consts.DbTablePrefix + "Tags", NewsManagement2Consts.DbSchema);
+            b.ConfigureByConvention();
+        });
+
+        builder.Entity<City>(b =>
+        {
+            b.HasMany(x => x.ListableContentCities).WithOne(x => x.City).HasForeignKey(x => x.CityId).IsRequired();
+
+            b.ToTable(NewsManagement2Consts.DbTablePrefix + "Cities", NewsManagement2Consts.DbSchema);
+            b.ConfigureByConvention();
+        });
+
+        builder.Entity<Category>(b =>
+        {
+            b.HasMany(x => x.ListableContentCategories).WithOne(x => x.Category).HasForeignKey(x => x.CategoryId).IsRequired();
+
+            b.ToTable(NewsManagement2Consts.DbTablePrefix + "Categories", NewsManagement2Consts.DbSchema);
+            b.ConfigureByConvention();
+        });
+        #endregion
     }
 }
