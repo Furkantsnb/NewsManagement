3d1a3237b224d64fdea798d4a300d4439696049b
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-11-12 17:05:32 +0300

    DataSeeder sınıfı oluşturuldu

---
+++ src/NewsManagement2.Domain/NewsManagementDataSeederContributor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+
+namespace NewsManagement2
+{
+    internal class NewsManagementDataSeederContributor : IDataSeedContributor, ITransientDependency
+    {
+        public Task SeedAsync(DataSeedContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task SeedTenantAsync()
+        { 
+        
+        }
+
+        }
+}
