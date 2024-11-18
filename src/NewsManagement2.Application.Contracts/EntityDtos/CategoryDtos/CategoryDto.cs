9d02c466d4f445a7d5e2495bf4b8b06b49e7c523
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-11-18 16:36:26 +0300

    CategotyDto

---
+++ src/NewsManagement2.Application.Contracts/EntityDtos/CategoryDtos/CategoryDto.cs
@@ -0,0 +1,19 @@
+﻿using NewsManagement2.EntityConsts.ListableContentConsts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+
+namespace NewsManagement2.EntityDtos.CategoryDtos
+{
+    public class CategoryDto: FullAuditedEntityDto<int>
+    {
+        public string CategoryName { get; set; }
+        public string ColorCode { get; set; }
+        public bool IsActive { get; set; }
+        public int? ParentCategoryId { get; set; }
+        public ListableContentType listableContentType { get; set; }
+    }
+
+   
+}
