76208fc551b9e3f7c920795269ee5a228b792be7
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-11-18 14:37:50 +0300

    ITagAppService

---
+++ src/NewsManagement2.Application.Contracts/EntityDtos/TagDtos/ITagAppService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+
+
+
+namespace NewsManagement2.EntityDtos.TagDtos
+{
+    public interface ITagAppService : ICrudAppService<TagDto, int, CreateTagDto, UpdateTagDto>
+    {
+        Task DeleteHardAsync(int id);
+    }
+}
