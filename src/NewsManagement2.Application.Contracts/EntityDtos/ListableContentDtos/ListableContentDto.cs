984a3e43358dccdb2f41eda4bb9217af9bf4ddff
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-11-18 17:05:38 +0300

    ListableContentDto

---
+++ src/NewsManagement2.Application.Contracts/EntityDtos/ListableContentDtos/ListableContentDto.cs
@@ -0,0 +1,20 @@
+﻿using NewsManagement2.EntityConsts.ListableContentConsts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+
+namespace NewsManagement2.EntityDtos.ListableContentDtos
+{
+    public class ListableContentDto : FullAuditedEntityDto<int>
+    {
+        public string Title { get; set; }
+        public string Spot { get; set; }
+        public Guid? ImageId { get; set; }
+        public int ViewsCount { get; set; }
+        public StatusType Status { get; set; }
+        public DateTime? PublishTime { get; set; }
+        public ListableContentType ListableContentType { get; set; }
+       
+    }
+}
