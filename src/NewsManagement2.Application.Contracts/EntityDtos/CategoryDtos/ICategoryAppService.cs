b1dd57582314dddb56d963fe507b750074d15481
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-11-18 16:50:40 +0300

    ICategoryAppCervice

---
+++ src/NewsManagement2.Application.Contracts/EntityDtos/CategoryDtos/ICategoryAppService.cs
@@ -0,0 +1,14 @@
+﻿using NewsManagement2.EntityDtos.CityDtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace NewsManagement2.EntityDtos.CategoryDtos
+{
+    internal interface ICategoryAppService : ICrudAppService<CategoryDto, int, CreateCategoryDto, UpdateCategoryDto>
+    {
+        Task DeleteHardAsync(int id);
+    }
+}
