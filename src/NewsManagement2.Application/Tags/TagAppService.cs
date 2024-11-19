e219dbd969c31ec2ff011771e0ded37e0bf26e86
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-11-19 10:34:13 +0300

    TagAppService

--- src/NewsManagement2.Application/Tags/TagAppService.cs
+++ src/NewsManagement2.Application/Tags/TagAppService.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using NewsManagement2.Entities.Tags;
+using NewsManagement2.EntityDtos.TagDtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
 
 namespace NewsManagement2.Tags
 {
-    internal class TagAppService
+    public class TagAppService : CrudAppService<Tag, TagDto, int, PagedAndSortedResultRequestDto, CreateTagDto, UpdateTagDto>, ITagAppService
     {
+        public TagAppService(IRepository<Tag, int> repository) : base(repository)
+        {
+        }
+
+        public Task DeleteHardAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
