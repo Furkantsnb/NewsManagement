11f3ae6ce375a4b8b98a6c6a818ea9c9fee9d93a
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-11-19 14:35:35 +0300

    EfCoreTagRepository

---
+++ src/NewsManagement2.EntityFrameworkCore/EntityRepositories/Tags/EfCoreTagRepository.cs
@@ -0,0 +1,23 @@
+﻿using NewsManagement2.Entities.Tags;
+using NewsManagement2.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace NewsManagement2.EntityRepositories.Tags
+{
+    internal class EfCoreTagRepository : EfCoreRepository<NewsManagement2DbContext, Tag, int>, ITagRepository
+    {
+        public EfCoreTagRepository(IDbContextProvider<NewsManagement2DbContext> dbContextProvider) : base(dbContextProvider)
+        {
+        }
+        public Task<List<Tag>> GetListAsync(int skipCount, int maxResultCount, string sorting, string filter = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
