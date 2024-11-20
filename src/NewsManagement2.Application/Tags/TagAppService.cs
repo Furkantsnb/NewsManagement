04c904390ab0aef2ca3a14204745dbe57ec91c4d
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-11-20 09:36:25 +0300

    Tag için Authorize işlemleri yapıldı

--- src/NewsManagement2.Application/Tags/TagAppService.cs
+++ src/NewsManagement2.Application/Tags/TagAppService.cs
@@ -1,6 +1,8 @@
-﻿using NewsManagement2.Entities.Tags;
+﻿using Microsoft.AspNetCore.Authorization;
+using NewsManagement2.Entities.Tags;
 using NewsManagement2.EntityDtos.PagedAndSortedDto;
 using NewsManagement2.EntityDtos.TagDtos;
+using NewsManagement2.Permissions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +14,7 @@ using Volo.Abp.Domain.Repositories;
 
 namespace NewsManagement2.Tags
 {
+    [Authorize(NewsManagement2Permissions.Tags.Default)]
     public class TagAppService : CrudAppService<Tag, TagDto, int, GetListPagedAndSortedDto, CreateTagDto, UpdateTagDto>, ITagAppService
     {
         private readonly TagManager _tagManager;
@@ -21,20 +24,25 @@ namespace NewsManagement2.Tags
             _tagManager = tagManager;
 
         }
+        [Authorize(NewsManagement2Permissions.Tags.Create)]
         public override async Task<TagDto> CreateAsync(CreateTagDto createTagDto)
         {
             return await _tagManager.CreateAsync(createTagDto);
         }
+        [Authorize(NewsManagement2Permissions.Tags.Edit)]
         public async override Task<TagDto> UpdateAsync(int id, UpdateTagDto input)
         {
             return await _tagManager.UpdateAsync(id, input);
         }
+        [Authorize(NewsManagement2Permissions.Tags.Delete)]
         public override async Task DeleteAsync(int id)
         {
             await _tagManager.DeleteAsync(id);
 
             await base.DeleteAsync(id);
         }
+
+        [Authorize(NewsManagement2Permissions.Tags.Delete)]
         public async Task DeleteHardAsync(int id)
         {
             await _tagManager.DeleteHardAsync(id);
