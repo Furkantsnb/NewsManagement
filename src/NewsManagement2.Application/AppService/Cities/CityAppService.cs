19208e384f5778faac714fb81d66faa8a964fe51
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-11-21 16:51:08 +0300

    Authorize işlemleri yapıldı

--- src/NewsManagement2.Application/AppService/Cities/CityAppService.cs
+++ src/NewsManagement2.Application/AppService/Cities/CityAppService.cs
@@ -1,6 +1,8 @@
-﻿using NewsManagement2.Entities.Cities;
+﻿using Microsoft.AspNetCore.Authorization;
+using NewsManagement2.Entities.Cities;
 using NewsManagement2.EntityDtos.CityDtos;
 using NewsManagement2.EntityDtos.PagedAndSortedDto;
+using NewsManagement2.Permissions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +14,7 @@ using Volo.Abp.Domain.Repositories;
 
 namespace NewsManagement2.AppService.Cities
 {
+    [Authorize(NewsManagement2Permissions.Cities.Default)]
     public class CityAppService : CrudAppService<City, CityDto, int, GetListPagedAndSortedDto, CreateCityDto, UpdateCityDto>, ICityAppService
     {
         private readonly CityManager _cityManager;
@@ -20,10 +23,12 @@ namespace NewsManagement2.AppService.Cities
         {
             _cityManager = cityManager;
         }
+        [Authorize(NewsManagement2Permissions.Cities.Create)]
         public override async Task<CityDto> CreateAsync(CreateCityDto createCityDto)
         {
             return await _cityManager.CreateAsync(createCityDto);
         }
+        [Authorize(NewsManagement2Permissions.Cities.Edit)]
         public async override Task<CityDto> UpdateAsync(int id, UpdateCityDto updateCityDto)
         {
             return await _cityManager.UpdateAsync(id, updateCityDto);
@@ -32,12 +37,15 @@ namespace NewsManagement2.AppService.Cities
         {
             return await _cityManager.GetListAsync(input);
         }
+        [Authorize(NewsManagement2Permissions.Cities.Delete)]
         public override async Task DeleteAsync(int id)
         {
             await _cityManager.DeleteAsync(id);
 
             await base.DeleteAsync(id);
         }
+
+        [Authorize(NewsManagement2Permissions.Cities.Delete)]
         public async Task DeleteHardAsync(int id)
         {
             await _cityManager.DeleteHardAsync(id);
