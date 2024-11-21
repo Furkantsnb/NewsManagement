cff866348cdfe4057c3a9ad1dbb434f3eba41324
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-11-21 16:39:16 +0300

    City

--- src/NewsManagement2.Application/NewsManagement2ApplicationAutoMapperProfile.cs
+++ src/NewsManagement2.Application/NewsManagement2ApplicationAutoMapperProfile.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
+using NewsManagement2.Entities.Cities;
 using NewsManagement2.Entities.Tags;
+using NewsManagement2.EntityDtos.CityDtos;
 using NewsManagement2.EntityDtos.TagDtos;
 
 namespace NewsManagement2;
@@ -13,5 +15,12 @@ public class NewsManagement2ApplicationAutoMapperProfile : Profile
         CreateMap<UpdateTagDto, Tag>().ReverseMap();
         CreateMap<CreateTagDto, Tag>().ReverseMap();
         #endregion
+
+        #region City
+        CreateMap<City, CityDto>().ReverseMap();
+        CreateMap<UpdateCityDto, City>().ReverseMap();
+        CreateMap<CreateCityDto, City>().ReverseMap();
+        #endregion
     }
+
 }
