974ca75410a97d4f8eb4d01eb48cfcf905b6b955
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-11-24 21:37:18 +0300

    Constructor Injection

--- src/NewsManagement2.Domain/Entities/Categories/CategoryManager.cs
+++ src/NewsManagement2.Domain/Entities/Categories/CategoryManager.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Volo.Abp.Data;
+using Volo.Abp;
+using Volo.Abp.Domain.Services;
+using Volo.Abp.ObjectMapping;
 
 namespace NewsManagement2.Entities.Categories
 {
-    public class CategoryManager
+    public class CategoryManager : DomainService
     {
+        private readonly IObjectMapper _objectMapper;
+        private readonly IDataFilter<ISoftDelete> _softDeleteFilter;
+
+        public CategoryManager(IObjectMapper objectMapper, IDataFilter<ISoftDelete> softDeleteFilter)
+        {
+            _objectMapper = objectMapper;
+            _softDeleteFilter = softDeleteFilter;
+        }
     }
 }
