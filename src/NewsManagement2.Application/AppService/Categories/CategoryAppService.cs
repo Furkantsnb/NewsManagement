d3f8747eeb04f51c39cc47e03fe982ba96214d8b
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-03 13:18:17 +0300

    .

--- src/NewsManagement2.Application/AppService/Categories/CategoryAppService.cs
+++ src/NewsManagement2.Application/AppService/Categories/CategoryAppService.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authorization;
+using NewsManagement2.Entities.Categories;
+using NewsManagement2.EntityDtos.CategoryDtos;
+using NewsManagement2.EntityDtos.PagedAndSortedDto;
+using NewsManagement2.Permissions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
 
 namespace NewsManagement2.AppService.Categories
 {
-    internal class CategoryAppService
+    [Authorize(NewsManagement2Permissions.Categories.Default)]
+    public class CategoryAppService : CrudAppService<Category, CategoryDto, int, GetListPagedAndSortedDto, CreateCategoryDto, UpdateCategoryDto>, ICategoryAppService
     {
+        private readonly CategoryManager _categoryManager;
+
+        public CategoryAppService(IRepository<Category, int> repository, CategoryManager categoryManager) : base(repository)
+        {
+            _categoryManager = categoryManager;
+        }
+
+
+
     }
 }
