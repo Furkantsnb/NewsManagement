865351f1e01b8e1d1de4558592f6d91bfcc567d1
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-09 23:15:42 +0300

    .

--- src/NewsManagement2.Domain/Entities/ListableContents/IListableContentRepository.cs
+++ src/NewsManagement2.Domain/Entities/ListableContents/IListableContentRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories;
 
 namespace NewsManagement2.Entities.ListableContents
 {
     public interface IListableContentRepository : IRepository<ListableContent, int>
     {
+        Task<List<ListableContent>> GetListFilterAsync(int skipCount, int maxResultCount, string sorting, string filter = null);
     }
 }
