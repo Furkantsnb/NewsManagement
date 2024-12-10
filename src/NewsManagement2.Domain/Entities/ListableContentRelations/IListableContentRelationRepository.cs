1da868717028b8c05dfa64ae3d15a7e77cd4e1f7
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-10 13:57:32 +0300

    IListableContentRelationRepository

---
+++ src/NewsManagement2.Domain/Entities/ListableContentRelations/IListableContentRelationRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories;
+
+namespace NewsManagement2.Entities.ListableContentRelations
+{
+    public interface IListableContentRelationRepository : IRepository<ListableContentRelation>
+    {
+        Task<List<ListableContentRelation>> GetCrossListAsync(int id);
+    }
+}
