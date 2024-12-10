3ade48cd522918a8fc3fdd51f935befddf2bfb64
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-10 15:12:37 +0300

    EfCoreListableContentRelationRepository

---
+++ src/NewsManagement2.EntityFrameworkCore/EntityRepositories/ListableContentRelations/EfCoreListableContentRelationRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewsManagement2.Entities.ListableContentRelations;
+using NewsManagement2.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace NewsManagement2.EntityRepositories.ListableContentRelations
+{
+    public class EfCoreListableContentRelationRepository : EfCoreRepository<NewsManagement2DbContext, ListableContentRelation>, IListableContentRelationRepository
+    {
+        public EfCoreListableContentRelationRepository(IDbContextProvider<NewsManagement2DbContext> dbContextProvider) : base(dbContextProvider)
+        {
+        }
+
+        public async Task<List<ListableContentRelation>> GetCrossListAsync(int id)
+        {
+            var dbSet = await GetDbSetAsync();
+
+            return await dbSet.Where(x => x.ListableContentId == id)
+              .Include(x => x.RelatedListableContent).ToListAsync();
+        }
+    }
+}
