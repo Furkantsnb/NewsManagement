39c744d65832eb27af6576318385d3a5eb79cf50
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-11 18:14:41 +0300

    EfCoreNewsRepository

---
+++ src/NewsManagement2.EntityFrameworkCore/EntityRepositories/News/EfCoreNewsRepository.cs
@@ -0,0 +1,22 @@
+﻿using NewsManagement2.Entities.ListableContents;
+using NewsManagement2.Entities.Newses;
+using NewsManagement2.EntityFrameworkCore;
+using NewsManagement2.EntityRepositories.ListableContents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace NewsManagement2.EntityRepositories.News
+{
+    public class EfCoreNewsRepository: EfCoreListableContentGenericRepository<NewsManagement2.Entities.Newses.News>, INewsRepository, IListableContentGenericRepository<NewsManagement2.Entities.Newses.News>
+    {
+        public EfCoreNewsRepository(IDbContextProvider<NewsManagement2DbContext> dbContextProvider) : base(dbContextProvider)
+        {
+        }
+
+
+    }
+}
