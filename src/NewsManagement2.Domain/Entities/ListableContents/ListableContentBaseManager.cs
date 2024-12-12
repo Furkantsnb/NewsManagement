56f6e84e2bc07f03502412a4b51bbe2e228be97c
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-12 19:09:22 +0300

    ListableContentBaseManager sınıfı için bağımlılıklar eklendi

--- src/NewsManagement2.Domain/Entities/ListableContents/ListableContentBaseManager.cs
+++ src/NewsManagement2.Domain/Entities/ListableContents/ListableContentBaseManager.cs
@@ -1,4 +1,12 @@
-﻿using NewsManagement2.EntityDtos.ListableContentDtos;
+﻿using EasyAbp.FileManagement.Files;
+using NewsManagement2.Entities.Categories;
+using NewsManagement2.Entities.Cities;
+using NewsManagement2.Entities.Galleries;
+using NewsManagement2.Entities.ListableContentRelations;
+using NewsManagement2.Entities.Newses;
+using NewsManagement2.Entities.Tags;
+using NewsManagement2.Entities.Videos;
+using NewsManagement2.EntityDtos.ListableContentDtos;
 using NewsManagement2.EntityDtos.PagedAndSortedDto;
 using System;
 using System.Collections.Generic;
@@ -6,6 +14,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Volo.Abp.Domain.Services;
+using Volo.Abp.ObjectMapping;
 
 namespace NewsManagement2.Entities.ListableContents
 {
@@ -25,5 +34,56 @@ namespace NewsManagement2.Entities.ListableContents
         where TEntityUpdateDto : UpdateListableContentDto
         where TPagedDto : GetListPagedAndSortedDto
     {
+        #region Bağımlılıklar (Dependencies)
+
+        private readonly IObjectMapper _objectMapper;
+        private readonly ITagRepository _tagRepository;
+        private readonly ICityRepository _cityRepository;
+        private readonly INewsRepository _newsRepository;
+        private readonly IVideoRepository _videoRepository;
+        private readonly IGalleryRepository _galleryRepository;
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IListableContentGenericRepository<TEntity> _genericRepository;
+        private readonly IListableContentTagRepository _listableContentTagRepository;
+        private readonly IListableContentCityRepository _listableContentCityRepository;
+        private readonly IListableContentCategoryRepository _listableContentCategoryRepository;
+        private readonly IListableContentRelationRepository _listableContentRelationRepository;
+        private readonly IFileRepository _fileRepository;
+
+        /// <summary>
+        /// ListableContentBaseManager sınıfını gerekli bağımlılıklar ile başlatır.
+        /// </summary>
+        protected ListableContentBaseManager(
+            IObjectMapper objectMapper,
+            ITagRepository tagRepository,
+            ICityRepository cityRepository,
+            INewsRepository newsRepository,
+            IVideoRepository videoRepository,
+            IGalleryRepository galleryRepository,
+            ICategoryRepository categoryRepository,
+            IListableContentGenericRepository<TEntity> genericRepository,
+            IListableContentTagRepository listableContentTagRepository,
+            IListableContentCityRepository listableContentCityRepository,
+            IListableContentCategoryRepository listableContentCategoryRepository,
+            IListableContentRelationRepository listableContentRelationRepository,
+            IFileRepository fileRepository
+        )
+        {
+            _objectMapper = objectMapper;
+            _tagRepository = tagRepository;
+            _cityRepository = cityRepository;
+            _newsRepository = newsRepository;
+            _videoRepository = videoRepository;
+            _galleryRepository = galleryRepository;
+            _genericRepository = genericRepository;
+            _categoryRepository = categoryRepository;
+            _listableContentTagRepository = listableContentTagRepository;
+            _listableContentCityRepository = listableContentCityRepository;
+            _listableContentCategoryRepository = listableContentCategoryRepository;
+            _listableContentRelationRepository = listableContentRelationRepository;
+            _fileRepository = fileRepository;
+        }
+
+        #endregion
     }
 }
