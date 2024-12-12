d4d073492d02f97a340f429c3ecd36405ff0e09a
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-12 19:00:58 +0300

    ListableContentBaseManager

---
+++ src/NewsManagement2.Domain/Entities/ListableContents/ListableContentBaseManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Services;
+
+namespace NewsManagement2.Entities.ListableContents
+{
+    public abstract class ListableContentBaseManager<TEntity, TEntityDto, TPagedDto, TEntityCreateDto, TEntityUpdateDto> : DomainService
+    {
+    }
+}
