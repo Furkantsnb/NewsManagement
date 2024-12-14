f27a1bce6a5667f379f46d43da70ba4c71a99a6e
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-14 23:41:56 +0300

    UpdateGalleryDto

---
+++ src/NewsManagement2.Application.Contracts/EntityDtos/GalleryDtos/UpdateGalleryDto.cs
@@ -0,0 +1,12 @@
+﻿using NewsManagement2.EntityDtos.ListableContentDtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsManagement2.EntityDtos.GalleryDtos
+{
+    public class UpdateGalleryDto : UpdateListableContentDto
+    {
+        public List<GalleryImageDto> GalleryImages { get; set; }
+    }
+}
