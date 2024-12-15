e8abd814cbc85e3a7c94baaed947d2b02a61b1c1
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-15 22:34:38 +0300

    CreateListableContentDtoValidator

--- src/NewsManagement2.Application.Contracts/FluentValidations/Gallery/CreateGalleryDtoValidator.cs
+++ src/NewsManagement2.Application.Contracts/FluentValidations/Gallery/CreateGalleryDtoValidator.cs
@@ -1,5 +1,7 @@
 ﻿using FluentValidation;
+using Microsoft.Extensions.Localization;
 using NewsManagement2.EntityDtos.GalleryDtos;
+using NewsManagement2.Localization;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,5 +10,6 @@ namespace NewsManagement2.FluentValidations.Gallery
 {
     public class CreateGalleryDtoValidator : AbstractValidator<CreateGalleryDto>
     {
+     
     }
 }
---
+++ src/NewsManagement2.Application.Contracts/FluentValidations/ListableContent/CreateListableContentDtoValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using NewsManagement2.EntityDtos.ListableContentDtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsManagement2.FluentValidations.ListableContent
+{
+    public class CreateListableContentDtoValidator : AbstractValidator<CreateListableContentDto>
+    {
+    }
+}
