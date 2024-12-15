8e9f9a72584b21bf5c56c85769be37f10b0e468a
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-15 23:23:02 +0300

    ListableContentCategoryDtoValidator

---
+++ src/NewsManagement2.Application.Contracts/FluentValidations/ListableContent/ListableContentCategoryDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using NewsManagement2.EntityDtos.ListableContentDtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsManagement2.FluentValidations.ListableContent
+{
+    public class ListableContentCategoryDtoValidator : AbstractValidator<ListableContentCategoryDto>
+    {
+        public ListableContentCategoryDtoValidator()
+        {
+            RuleFor(x => x.CategoryId).NotEmpty();
+
+        }
+    }
+}
