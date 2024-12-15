6e6afe0740f21f5b921b115e000f6aa1b30414e7
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-15 20:32:21 +0300

    IGalleryAppService

---
+++ src/NewsManagement2.Application.Contracts/EntityDtos/GalleryDtos/IGalleryAppService.cs
@@ -0,0 +1,14 @@
+﻿using NewsManagement2.EntityDtos.PagedAndSortedDto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace NewsManagement2.EntityDtos.GalleryDtos
+{
+    public interface IGalleryAppService : ICrudAppService<GalleryDto, int, GetListPagedAndSortedDto, CreateGalleryDto, UpdateGalleryDto>
+    {
+        Task DeleteHardAsync(int id);
+    }
+}
