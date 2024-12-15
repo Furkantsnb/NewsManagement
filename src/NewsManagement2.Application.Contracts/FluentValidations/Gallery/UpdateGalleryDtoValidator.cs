0ffda16002308ed4976d1d3f047fed3b17b2fba0
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-15 23:13:41 +0300

    UpdateGalleryDtoValidator

---
+++ src/NewsManagement2.Application.Contracts/FluentValidations/Gallery/UpdateGalleryDtoValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using NewsManagement2.EntityDtos.GalleryDtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsManagement2.FluentValidations.Gallery
+{
+    public class UpdateGalleryDtoValidator : AbstractValidator<UpdateGalleryDto>
+    {
+    }
+}
