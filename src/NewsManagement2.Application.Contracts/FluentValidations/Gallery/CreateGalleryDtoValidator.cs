21f7675fd040cfd812eac10e6eed4797272530ac
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-15 23:20:05 +0300

    gerekli validation işlemleri yapıldı

--- src/NewsManagement2.Application.Contracts/FluentValidations/Gallery/CreateGalleryDtoValidator.cs
+++ src/NewsManagement2.Application.Contracts/FluentValidations/Gallery/CreateGalleryDtoValidator.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation;
 using Microsoft.Extensions.Localization;
 using NewsManagement2.EntityDtos.GalleryDtos;
+using NewsManagement2.FluentValidations.ListableContent;
 using NewsManagement2.Localization;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,12 @@ namespace NewsManagement2.FluentValidations.Gallery
 {
     public class CreateGalleryDtoValidator : AbstractValidator<CreateGalleryDto>
     {
-     
+        public CreateGalleryDtoValidator(IStringLocalizer<NewsManagement2Resource> localizer)
+        {
+            Include(new CreateListableContentDtoValidator(localizer));
+
+            RuleFor(x => x.GalleryImages).ForEach(x => x.SetValidator(new GalleryImageDtoValidator()));
+
+        }
     }
 }
