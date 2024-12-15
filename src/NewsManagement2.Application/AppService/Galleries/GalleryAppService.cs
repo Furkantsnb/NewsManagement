d08dcc6d3c46d0539361d17d628570a17ab5ebfc
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-15 20:34:11 +0300

    GalleryAppService

---
+++ src/NewsManagement2.Application/AppService/Galleries/GalleryAppService.cs
@@ -0,0 +1,27 @@
+﻿using NewsManagement2.Entities.Galleries;
+using NewsManagement2.EntityDtos.GalleryDtos;
+using NewsManagement2.EntityDtos.PagedAndSortedDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace NewsManagement2.AppService.Galleries
+{
+    public class GalleryAppService : CrudAppService<Gallery, GalleryDto, int, GetListPagedAndSortedDto, CreateGalleryDto, UpdateGalleryDto>, IGalleryAppService
+    {
+        private readonly GalleryManager _galleryManager;
+
+        public GalleryAppService(IRepository<Gallery, int> repository, GalleryManager galleryManager) : base(repository)
+        {
+            _galleryManager = galleryManager;
+        }
+        public Task DeleteHardAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
