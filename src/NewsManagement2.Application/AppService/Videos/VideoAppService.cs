f44802679d7c481a35d897667548767f8defcb16
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-16 22:22:55 +0300

    VideoAppService oluşturuldu

---
+++ src/NewsManagement2.Application/AppService/Videos/VideoAppService.cs
@@ -0,0 +1,29 @@
+﻿using NewsManagement2.Entities.Videos;
+using NewsManagement2.EntityDtos.PagedAndSortedDto;
+using NewsManagement2.EntityDtos.VideoDtos;
+using NewsManagement2.EntityDtos.Videos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace NewsManagement2.AppService.Videos
+{
+    public class VideoAppService : CrudAppService<Video, VideoDto, int, GetListPagedAndSortedDto, CreateVideoDto, UpdateVideoDto>, IVideoAppService
+    {
+        private readonly VideoManager _videoManager;
+
+        public VideoAppService(IRepository<Video, int> repository, VideoManager videoManager) : base(repository)
+        {
+            _videoManager = videoManager;
+        }
+
+        public Task DeleteHardAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
