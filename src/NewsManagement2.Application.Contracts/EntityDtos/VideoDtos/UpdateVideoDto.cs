d2a47442afeaeeb9a3891ff6da3bd9e053423413
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-16 22:18:10 +0300

    UpdateVideoDto

---
+++ src/NewsManagement2.Application.Contracts/EntityDtos/VideoDtos/UpdateVideoDto.cs
@@ -0,0 +1,16 @@
+﻿using NewsManagement2.EntityConsts.VideoConsts;
+using NewsManagement2.EntityDtos.ListableContentDtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsManagement2.EntityDtos.VideoDtos
+{
+    public class UpdateVideoDto : UpdateListableContentDto
+    {
+        public VideoType VideoType { get; set; }
+        public string? Url { get; set; }
+        public Guid? VideoId { get; set; }
+
+    }
+}
