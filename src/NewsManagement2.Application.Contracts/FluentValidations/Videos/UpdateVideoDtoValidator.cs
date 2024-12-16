5d1421aaada8a33745523913270f206803eb9896
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-16 23:00:38 +0300

    UpdateVideoDtoValidator

---
+++ src/NewsManagement2.Application.Contracts/FluentValidations/Videos/UpdateVideoDtoValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Localization;
+using NewsManagement2.EntityDtos.VideoDtos;
+using NewsManagement2.FluentValidations.ListableContent;
+using NewsManagement2.Localization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsManagement2.FluentValidations.Videos
+{
+    public class UpdateVideoDtoValidator : AbstractValidator<UpdateVideoDto>
+    {
+        public UpdateVideoDtoValidator(IStringLocalizer<NewsManagement2Resource> localizer)
+        {
+            Include(new UpdateListableContentDtoValidator(localizer));
+
+            RuleFor(v => v.VideoType).IsInEnum().WithMessage(localizer[NewsManagement2DomainErrorCodes.VideoEnumValidationFailed]);
+        }
+    }
+}
