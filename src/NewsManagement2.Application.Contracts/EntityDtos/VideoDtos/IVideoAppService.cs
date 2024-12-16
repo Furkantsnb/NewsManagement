ab68b53e557cdda422551c102b5e269d8808af40
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-16 22:19:51 +0300

    IVideoAppService

---
+++ src/NewsManagement2.Application.Contracts/EntityDtos/VideoDtos/IVideoAppService.cs
@@ -0,0 +1,15 @@
+﻿using NewsManagement2.EntityDtos.PagedAndSortedDto;
+using NewsManagement2.EntityDtos.Videos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace NewsManagement2.EntityDtos.VideoDtos
+{
+    public interface IVideoAppService : ICrudAppService<VideoDto, int, GetListPagedAndSortedDto, CreateVideoDto, UpdateVideoDto>
+    {
+        Task DeleteHardAsync(int id);
+    }
+}
