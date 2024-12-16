aa159d1ba761f72200f78dc7e3a4739da42c0e35
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-16 22:59:40 +0300

    CreateVideoDtoValidator

---
+++ src/NewsManagement2.Application.Contracts/FluentValidations/Videos/CreateVideoDtoValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Localization;
+using NewsManagement2.EntityDtos.VideoDtos;
+using NewsManagement2.FluentValidations.ListableContent;
+using NewsManagement2.Localization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsManagement2.FluentValidations.Videos
+{
+    public class CreateVideoDtoValidator : AbstractValidator<CreateVideoDto>
+    {
+        public CreateVideoDtoValidator(IStringLocalizer<NewsManagement2Resource> localizer)
+        {
+            Include(new CreateListableContentDtoValidator(localizer));
+
+            RuleFor(v => v.VideoType).IsInEnum().WithMessage(localizer[NewsManagement2DomainErrorCodes.VideoEnumValidationFailed]);
+        }
+    }
+}
