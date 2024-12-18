a42ad1c356eb9aaab595f6a5a4d7ed4e7a69e40c
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-18 11:41:14 +0300

    NewsDto

---
+++ src/NewsManagement2.Application.Contracts/EntityDtos/Newses/NewsDto.cs
@@ -0,0 +1,12 @@
+﻿using NewsManagement2.EntityDtos.ListableContentDtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsManagement2.EntityDtos.Newses
+{
+    public class NewsDto : ListableContentDto
+    {
+        public List<Guid> DetailImageId { get; set; }
+    }
+}
