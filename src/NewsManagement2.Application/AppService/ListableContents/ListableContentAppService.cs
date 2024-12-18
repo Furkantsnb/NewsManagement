d3be1a1143c73322c32674d99f18b5524d9dba6b
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-18 14:42:53 +0300

    ListableContentAppService

---
+++ src/NewsManagement2.Application/AppService/ListableContents/ListableContentAppService.cs
@@ -0,0 +1,25 @@
+﻿using NewsManagement2.Entities.ListableContents;
+using NewsManagement2.EntityDtos.ListableContentDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsManagement2.AppService.ListableContents
+{
+    public class ListableContentAppService : NewsManagement2AppService, IListableContentAppService
+    {
+        private readonly ListableContentManager _listableContentManager;
+
+        public ListableContentAppService(ListableContentManager listableContentManager)
+        {
+            _listableContentManager = listableContentManager;
+        }
+
+        public Task<ListableContentDto> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
