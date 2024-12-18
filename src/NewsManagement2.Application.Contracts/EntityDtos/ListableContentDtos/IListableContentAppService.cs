3b9528681be871aacd5b1b9c2b6bea38cbbaab79
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-18 14:41:23 +0300

    IListableContentAppService

---
+++ src/NewsManagement2.Application.Contracts/EntityDtos/ListableContentDtos/IListableContentAppService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace NewsManagement2.EntityDtos.ListableContentDtos
+{
+    public interface IListableContentAppService : IApplicationService
+    {
+        Task<ListableContentDto> GetByIdAsync(int id);
+    }
+}
