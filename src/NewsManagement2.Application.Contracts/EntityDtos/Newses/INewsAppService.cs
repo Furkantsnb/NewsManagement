fb7f148c8e1c208985dfa1e08a06c6289056c3cd
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-18 11:50:47 +0300

    INewsAppService

---
+++ src/NewsManagement2.Application.Contracts/EntityDtos/Newses/INewsAppService.cs
@@ -0,0 +1,14 @@
+﻿using NewsManagement2.EntityDtos.PagedAndSortedDto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace NewsManagement2.EntityDtos.Newses
+{
+    public interface INewsAppService:ICrudAppService<NewsDto,int,GetListPagedAndSortedDto,CreateNewsDto,UpdateNewsDto>
+    {
+        Task DeleteHardAsync(int id);
+    }
+}
