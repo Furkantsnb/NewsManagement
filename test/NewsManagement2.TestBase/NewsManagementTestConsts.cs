6e14d43fea9abf09d3bfad01a68daed6741baff6
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-23 13:56:00 +0300

    NewsManagementTestConsts

---
+++ test/NewsManagement2.TestBase/NewsManagementTestConsts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsManagement2
+{
+    public class NewsManagementTestConsts
+    {
+        public const string CollectionDefinitionName = "NewsManagementTestCollection";
+
+    }
+}
