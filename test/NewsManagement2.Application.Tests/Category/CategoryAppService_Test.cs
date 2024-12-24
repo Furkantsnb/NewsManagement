14f9a647d21be647c33acf6d00689a05b4a25ec1
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-24 14:19:10 +0300

    CategoryAppService_Test sınıfı oluşturuldu ve bağımlılıklar eklendi

---
+++ test/NewsManagement2.Application.Tests/Category/CategoryAppService_Test.cs
@@ -0,0 +1,23 @@
+﻿using NewsManagement2.AppService.Categories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Data;
+using Volo.Abp.MultiTenancy;
+
+namespace NewsManagement2.Category
+{
+    public class CategoryAppService_Test : NewsManagement2ApplicationTestBase
+    {
+        private readonly CategoryAppService _categoryAppService;
+        private readonly IDataFilter<IMultiTenant> _dataFilter;
+
+        public CategoryAppService_Test()
+        {
+            _categoryAppService = GetRequiredService<CategoryAppService>();
+            _dataFilter = GetRequiredService<IDataFilter<IMultiTenant>>();
+        }
+    }
+}
