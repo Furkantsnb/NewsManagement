a21c7126c44c17ca0213674a70019163cfc22922
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-26 00:35:34 +0300

    .

---
+++ src/NewsManagement2.Application.Contracts/FluentValidations/Newses/UpdateNewsDtoValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Localization;
+using NewsManagement2.EntityDtos.Newses;
+using NewsManagement2.FluentValidations.ListableContent;
+using NewsManagement2.Localization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsManagement2.FluentValidations.Newses
+{
+    public class UpdateNewsDtoValidator : AbstractValidator<UpdateNewsDto>
+    {
+        public UpdateNewsDtoValidator(IStringLocalizer<NewsManagement2Resource> localizer)
+        {
+            Include(new UpdateListableContentDtoValidator(localizer));
+
+            RuleFor(x => x.DetailImageIds).ForEach(x => x.SetValidator(new NewsDetailImageDtoValidator()));
+        }
+    }
+}
