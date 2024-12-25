1de0cff563178f8e728792ed21b2a460227864ed
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-26 00:09:18 +0300

    gerekli bağımlılıklar eklendi

--- test/NewsManagement2.Application.Tests/ListableContent/ListableContentAppService_Test.cs
+++ test/NewsManagement2.Application.Tests/ListableContent/ListableContentAppService_Test.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using NewsManagement2.AppService.ListableContents;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Volo.Abp.Data;
+using Volo.Abp.MultiTenancy;
 
 namespace NewsManagement2.ListableContent
 {
     public class ListableContentAppService_Test : NewsManagement2ApplicationTestBase
     {
+        private readonly ListableContentAppService _listableContentAppService;
+        private readonly IDataFilter<IMultiTenant> _dataFilter;
+
+        public ListableContentAppService_Test()
+        {
+            _listableContentAppService = GetRequiredService<ListableContentAppService>();
+            _dataFilter = GetRequiredService<IDataFilter<IMultiTenant>>();
+        }
     }
 }
