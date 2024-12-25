0cf1d942e91103530cb180828097c50f63726587
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-25 20:56:46 +0300

    CityAppService_Test sınıfı oluşturuldu ve gerekli bağımlılıklar eklendi

---
+++ test/NewsManagement2.Application.Tests/City/CityAppService_Test.cs
@@ -0,0 +1,22 @@
+﻿using NewsManagement2.AppService.Cities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Data;
+using Volo.Abp.MultiTenancy;
+
+namespace NewsManagement2.City
+{
+    public class CityAppService_Test: NewsManagement2ApplicationTestBase
+    {
+        private readonly CityAppService _cityAppService;
+        private readonly IDataFilter<IMultiTenant> _dataFilter;
+        public CityAppService_Test()
+        {
+            _cityAppService = GetRequiredService<CityAppService>();
+            _dataFilter = GetRequiredService<IDataFilter<IMultiTenant>>();
+        }
+    }
+}
