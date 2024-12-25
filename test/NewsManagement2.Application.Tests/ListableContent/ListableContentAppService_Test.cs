366454e9113d27f31ea201b46667da93a3cc16eb
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-26 00:08:51 +0300

    ListableContentAppService_Test

---
+++ test/NewsManagement2.Application.Tests/ListableContent/ListableContentAppService_Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsManagement2.ListableContent
+{
+    public class ListableContentAppService_Test : NewsManagement2ApplicationTestBase
+    {
+    }
+}
