dc9e44aade7d0639f91440901397fa4ce2a74ebd
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-28 23:00:20 +0300

    GalleryAppService_Test

---
+++ test/NewsManagement2.Application.Tests/Gallery/GalleryAppService_Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsManagement2.Gallery
+{
+    public class GalleryAppService_Test : NewsManagement2ApplicationTestBase
+    {
+    }
+}
