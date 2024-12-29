d9c5313c9bf195176cae5c71a2b564f7e00554b3
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-12-29 11:47:19 +0300

    .

--- src/NewsManagement2.EntityFrameworkCore/EntityRepositories/Galleries/EfCoreGalleryRepository.cs
+++ src/NewsManagement2.EntityFrameworkCore/EntityRepositories/Galleries/EfCoreGalleryRepository.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using NewsManagement2.Entities.Galleries;
+using NewsManagement2.Entities.ListableContents;
+using NewsManagement2.EntityFrameworkCore;
+using NewsManagement2.EntityRepositories.ListableContents;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Volo.Abp.EntityFrameworkCore;
 
 namespace NewsManagement2.EntityRepositories.Galleries
 {
-    public class EfCoreGalleryRepository
+    public class EfCoreGalleryRepository : EfCoreListableContentGenericRepository<Gallery>, IGalleryRepository, IListableContentGenericRepository<Gallery>
     {
+        public EfCoreGalleryRepository(IDbContextProvider<NewsManagement2DbContext> dbContextProvider) : base(dbContextProvider)
+        {
+        }
+
+
     }
 }
